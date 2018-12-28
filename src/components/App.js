fa243b39698893a0edea6be415fd0c665f721462
jv88899 <jv88899@gmail.com>
2018-12-28 17:57:19 -0600

    feat(drum-machine-app) add state object

---
+++ src/components/App.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    state = {
+        drumpad: [
+            {
+                keyCode: 81,
+                keyLetter: 'Q',
+                audioLink: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+                id: 'heater-1'
+            },
+            {
+                keyCode: 87,
+                keyLetter: 'W',
+                audioLink: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+                id: 'heater-2'
+            },
+            {
+                keyCode: 69,
+                keyLetter: 'E',
+                audioLink: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+                id: 'heater-3'
+            },
+            {
+                keyCode: 65,
+                keyLetter: 'A',
+                audioLink: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
+                id: 'heater-4'
+            },
+            {
+                keyCode: 83,
+                keyLetter: 'S',
+                audioLink: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+                id: 'heater-6'
+                
+            },
+            {
+                keyCode: 68,
+                keyLetter: 'D',
+                audioLink: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+                id: 'dsc-0h'
+            },
+            {
+                keyCode: 90,
+                keyLetter: 'Z',
+                audioLink: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+                id: 'kick-n-hat'
+            },
+            {
+                keyCode: 88,
+                keyLetter: 'X',
+                audioLink: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+                id: 'kick'
+            },
+            {
+                keyCode: 67,
+                keyLetter: 'C',
+                audioLink: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+                id: 'cev-h2'
+            }
+        ]
+    }
+
+    render() {
+        return (
+            <div id="drum-machine">
+                <div id="display"></div>
+            </div>
+        )
+    }
+}
+
+export default App;
